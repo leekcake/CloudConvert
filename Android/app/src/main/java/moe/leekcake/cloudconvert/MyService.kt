e25ea5390b9628d27a430e2947028c766cf4bd37
Blue Electric <be@leekcake.moe>
2020-08-12 05:28:42 +0900

    Android: Prevent crash on error

--- Android/app/src/main/java/moe/leekcake/cloudconvert/MyService.kt
+++ Android/app/src/main/java/moe/leekcake/cloudconvert/MyService.kt
@@ -6,7 +6,9 @@ import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
 import android.os.Build
+import android.os.Handler
 import android.os.IBinder
+import android.os.Looper
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import com.arthenica.mobileffmpeg.Config
@@ -23,14 +25,14 @@ class MyService : Service() {
         var readed: Int
         val buf = ByteArray(1024)
         while (left != 0) {
-            if(count == -1) {
+            if (count == -1) {
                 readed = src.read(buf, 0, 1024)
             } else {
                 readed = src.read(buf, 0, Math.min(1024, left))
             }
 
             if (readed == -1) {
-                if( count == -1 ) {
+                if (count == -1) {
                     break
                 }
                 try {
@@ -64,31 +66,40 @@ class MyService : Service() {
         }
     }
 
+    private var isPerformed = false
+
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        if (isPerformed) {
+            Log.i("CloudConvert", "Duplicate start command?")
+            return START_STICKY
+        }
+
+        isPerformed = true
         createNotificationChannel()
-        val notificationIntent = Intent(this, MainActivity::class.java)
-        val pendingIntent = PendingIntent.getActivity(
-            this,
-            0, notificationIntent, 0
-        )
+        val handler = Handler(Looper.getMainLooper())
+
+        val notiManager = getSystemService(NotificationManager::class.java)
         val builder = NotificationCompat.Builder(this, CHANNEL_ID)
             .setContentTitle("CloudConvert")
             .setContentText("Starting")
             .setSmallIcon(R.drawable.ic_launcher_foreground)
-            .setContentIntent(pendingIntent)
+
         startForeground(1, builder.build())
 
         fun makeLogAndUpdate(text: String) {
             Log.i("CloudConvert", text)
             builder.setContentText(text)
-            startForeground(1, builder.build())
+            handler.postDelayed({
+                notiManager.notify(1, builder.build())
+            }, 100)
         }
 
         val thread = Thread {
             val addr = "192.168.0.139"
-
+            Thread.sleep(1000)
             while (true) {
                 try {
+                    makeLogAndUpdate("서버와 연결 시도중...")
                     val socket: Socket
                     try {
                         socket = Socket(addr, 39000)
@@ -109,7 +120,7 @@ class MyService : Service() {
                         Thread.sleep(10000)
                         continue
                     }
-                    Out.write( "Yes!".toByteArray() )
+                    Out.write("Yes!".toByteArray())
 
                     while (true) {
                         makeLogAndUpdate("새 작업 대기")
@@ -118,20 +129,33 @@ class MyService : Service() {
                         makeLogAndUpdate("새 작업 요청, 변환 준비중")
                         val inputPipe = Config.registerNewFFmpegPipe(applicationContext)
                         val outputPipe = Config.registerNewFFmpegPipe(applicationContext)
-                        val executeLine = "-y -f mpegts -i $inputPipe -preset veryfast -c:v libx264 -c:a aac -f mpegts $outputPipe"
+                        val executeLine =
+                            "-y -f mpegts -i $inputPipe -preset veryfast -c:v libx264 -c:a aac -f mpegts $outputPipe"
                         Log.i("CloudConvert", executeLine)
                         val ffmpeg = Thread {
-                            FFmpeg.execute(executeLine)
+                            try {
+                                FFmpeg.execute(executeLine)
+                            } catch (ex: Exception) {
+                                ex.printStackTrace()
+                            }
                         }
                         ffmpeg.start()
 
                         //We need to provide to ffmpeg's "input"
                         //We need to get data from ffmpeg's "output"
                         val ffIn = FileOutputStream(inputPipe)
+                        var error = false
 
                         makeLogAndUpdate("작업 데이터 변환중")
                         val copy = Thread {
-                            copyStreamAndClose(In, ffIn, dataLen)
+                            try {
+                                copyStreamAndClose(In, ffIn, dataLen)
+                            } catch (ex: Exception) {
+                                error = true
+                                ex.printStackTrace()
+                                ffIn.close()
+                                ffmpeg.interrupt()
+                            }
                         }
                         copy.start()
 
@@ -140,7 +164,7 @@ class MyService : Service() {
 
                         var readed: Int
                         val buf = ByteArray(1024)
-                        while(true) {
+                        while (true) {
                             readed = ffOut.read(buf, 0, 1024)
                             if (readed == -1) {
                                 if (!ffmpeg.isAlive) {
@@ -159,11 +183,20 @@ class MyService : Service() {
                         ffOut.close()
                         Config.closeFFmpegPipe(inputPipe)
                         Config.closeFFmpegPipe(outputPipe)
+                        if(error) {
+                            makeLogAndUpdate("작업중 오류 발생, 연결을 끊고 재시도")
+                            try {
+                                socket.close()
+                            } catch (ex: java.lang.Exception) {
+
+                            }
+                            throw java.lang.Exception("Error on Convert")
+                        }
                         makeLogAndUpdate("작업 결과물 전송중")
                         val resultBA = result.toByteArray()
                         val bais = ByteArrayInputStream(resultBA)
 
-                        Out.write( "Done!".toByteArray() )
+                        Out.write("Done!".toByteArray())
                         Out.writeInt(resultBA.size)
                         copyStreamAndClose(bais, Out, resultBA.size, false)
                     }
@@ -171,12 +204,11 @@ class MyService : Service() {
                     ex.printStackTrace()
                     makeLogAndUpdate("예외: ${ex.message}")
                     Thread.sleep(10000)
-                    continue
                 }
             }
         }
         thread.start()
 
-        return START_NOT_STICKY
+        return START_STICKY
     }
 }
