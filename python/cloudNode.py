1ff272fa3336aca12c296977087197bfd803e32b
Blue Electric <be@leekcake.moe>
2020-08-12 05:29:17 +0900

    Python: Add retry on node

--- python/cloudNode.py
+++ python/cloudNode.py
@@ -40,32 +40,38 @@ class CloudNode:
         t.start()
 
     def _thread_client(self):
-        clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        clientSocket.connect((self.addr, 39000))
-
-        # Server is node-host?
-        handshake = recvall(clientSocket, 5).decode()
-        if handshake != "Node?":
-            logging.error(f"Handshake failure: {handshake}")
-            clientSocket.close()
-            return
-
-        clientSocket.sendall('Yes!'.encode())
-        logging.info(f"Connected to Host")
-
         while True:
-            logging.info(f"Receive Work Data")
-            dataLen = int.from_bytes(recvall(clientSocket, 4), byteorder='big')
+            try:
+                clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                clientSocket.connect((self.addr, 39000))
 
-            logging.info(f"Processing...")
-            p = subprocess.Popen(['ffmpeg', '-f', 'mpegts', '-i', '-',
-                                  '-c:v', 'libx264', '-c:a', 'aac', '-preset', 'veryfast', '-f', 'mpegts', '-'],
-                                 stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-            copy = threading.Thread(target=socketCopyToAndClose, args=(clientSocket, p.stdin, dataLen,))
-            copy.start()
-            converted = p.stdout.read()
+                # Server is node-host?
+                handshake = recvall(clientSocket, 5).decode()
+                if handshake != "Node?":
+                    logging.error(f"Handshake failure: {handshake}")
+                    clientSocket.close()
+                    return
 
-            logging.info(f"Sending Result Data")
-            clientSocket.sendall('Done!'.encode())
-            clientSocket.sendall(len(converted).to_bytes(4, byteorder='big'))
-            clientSocket.sendall(converted)
+                clientSocket.sendall('Yes!'.encode())
+                logging.info(f"Connected to Host")
+
+                while True:
+                    logging.info(f"Receive Work Data")
+                    dataLen = int.from_bytes(recvall(clientSocket, 4), byteorder='big')
+
+                    logging.info(f"Processing...")
+                    p = subprocess.Popen(['ffmpeg', '-f', 'mpegts', '-i', '-',
+                                          '-c:v', 'libx264', '-c:a', 'aac', '-preset', 'veryfast', '-f', 'mpegts', '-'],
+                                         stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+                    copy = threading.Thread(target=socketCopyToAndClose, args=(clientSocket, p.stdin, dataLen,))
+                    copy.start()
+                    converted = p.stdout.read()
+
+                    logging.info(f"Sending Result Data")
+                    clientSocket.sendall('Done!'.encode())
+                    clientSocket.sendall(len(converted).to_bytes(4, byteorder='big'))
+                    clientSocket.sendall(converted)
+            except Exception as ex:
+                print(ex)
+                print("Retry after 10 seconds")
+                time.sleep(10)
